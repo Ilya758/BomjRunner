e5c6d8a46277e53da9b3b36adf4a40378a0d0d7f
Dzmitry Iliukovich <mr.ilyukovich@gmail.com>
2022-03-02 22:34:24 +0300

    feat: add base settingsPage

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -8,9 +8,10 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { GreetingPage } from './pages/greeting/greeting.page';
 import { MainButtonsComponent } from './pages/main-buttons/main-buttons';
+import { SettingsComponent } from './pages/settings/settings';
 
 @NgModule({
-  declarations: [AppComponent, GreetingPage, MainButtonsComponent],
+  declarations: [AppComponent, GreetingPage, MainButtonsComponent, SettingsComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
